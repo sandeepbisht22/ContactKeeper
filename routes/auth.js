6306aaffa53bf089545fdc999fdcf3d1288bb2fe
Sandeep <bishtsandy22@gmail.com>
2021-09-14 09:23:43 +0530

    routes updated

--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,11 @@ router.get("/", (req, res) => {
   res.send("Get Logged in user");
 });
 
+//@route    POST api/auth
+//@desc     Get logged in user
+//@access   Public
+router.post("/", (req, res) => {
+  res.send("Log in user");
+});
+
 module.exports = router;
