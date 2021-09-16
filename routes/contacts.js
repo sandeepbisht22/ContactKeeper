766fd8fe0b8d0e452bfd4201f0ad98b502e39aa0
Sandeep <bishtsandy22@gmail.com>
2021-09-16 10:40:59 +0530

    Adding new cotacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -23,9 +23,32 @@ router.get("/", auth, async (req, res) => {
 //@route    POST api/contacts
 //@desc     Add contact
 //@access   Private
-router.post("/", (req, res) => {
-  res.send("Add contact");
-});
+router.post(
+  "/",
+  [auth, [body("name", "Please add name").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const { name, email, phone, type } = req.body;
+      const newContact = Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+      const contact = await newContact.save();
+
+      return res.json({ contact });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 //@route    PUT api/contacts/:id
 //@desc     Update contact
