1e9e06e510d32d58e3589361f85fed0b251400b9
Sandeep <bishtsandy22@gmail.com>
2021-09-17 11:36:54 +0530

    Clean up, added baisc front dependencies and router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import { Fragment } from "react";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div class="container">
+          <switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/about" component={About}></Route>
+          </switch>
+        </div>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
