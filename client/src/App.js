c28837160243408d0c8bc3bdb37295edb8a380ae
Sandeep <bishtsandy22@gmail.com>
2021-09-18 23:14:03 +0530

    Baisc contact context setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,22 @@ import Navbar from "./components/layout/Navbar";
 import { Fragment } from "react";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-
+import ContactState from "./context/contact/Contactstate";
 const App = () => {
   return (
-    <Router>
-      <Fragment className="App">
-        <Navbar />
-        <div class="container">
-          <switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path="/about" component={About}></Route>
-          </switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment className="App">
+          <Navbar />
+          <div class="container">
+            <switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/about" component={About}></Route>
+            </switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,68 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CONTACT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Sandeep",
+        email: "bishtsandy22@gmail.com",
+        phone: "111-1111-111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "bhai",
+        email: "bhai@gmail.com",
+        phone: "222-222-2222",
+        type: "personal",
+      },
+      {
+        id: 1,
+        name: "mummy",
+        email: "mummy@gmail.com",
+        phone: "3333-333-3333",
+        type: "professional",
+      },
+    ],
+  };
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //ADD contact
+
+  //Delete contact
+
+  //Set current contact
+
+  //Clear current contact
+
+  //update contact
+
+  //filter contact
+
+  //clear contact
+
+  return (
+    <ContactContext.Provider>
+      value=
+      {{
+        contacts: state.contacts,
+      }}
+      {props.childern}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
