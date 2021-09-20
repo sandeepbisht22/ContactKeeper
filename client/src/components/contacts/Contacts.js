905da26cae6e3d101232e7259394ee820e2d1555
Sandeep <bishtsandy22@gmail.com>
2021-09-20 09:52:27 +0530

    added cotactitem which will take contact one by one and display it

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,12 +1,13 @@
 import React, { Fragment, useContext } from "react";
 import ContactContext from "../../context/contact/contactContext";
+import ContactsItem from "./ContactsItem";
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
   const { contacts } = contactContext;
   return (
     <Fragment>
       {contacts.map((contact) => (
-        <h3>{contact.name}</h3>
+        <ContactsItem key={contact.id} contact={contact}></ContactsItem>
       ))}
     </Fragment>
   );
