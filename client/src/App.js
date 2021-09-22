e6f7c85143eeab0d2fc8af7b2bf0e855d4597a87
Sandeep <bishtsandy22@gmail.com>
2021-09-22 09:42:45 +0530

    initial auth context setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,25 @@ import { Fragment } from "react";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment className="App">
-          <Navbar />
-          <div class="container">
-            <switch>
-              <Route exact path="/" component={Home}></Route>
-              <Route exact path="/about" component={About}></Route>
-            </switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment className="App">
+            <Navbar />
+            <div class="container">
+              <switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/about" component={About}></Route>
+              </switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,49 @@
+import React, { useContext, useReducer } from "react";
+import AuthContext from "./authContext";
+import authReducer from "./authReducer";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from "../types";
+const AuthState = (props) => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    error: null,
+    user: null,
+  };
+
+  //load user
+
+  //Register USer
+
+  //Login User
+
+  //Logout
+
+  //clear error
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
